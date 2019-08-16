478059afa931829b2c0fc539d33b4cdbac54de30
samuelbarrett1234 <samuelbarrett1234@btinternet.com>
2019-08-16 10:23:39 +0100

    Implemented end phase command.

---
+++ Core40KLearn/EndPhaseCommand.cpp
@@ -0,0 +1,128 @@
+#include "EndPhaseCommand.h"
+#include "GameState.h"
+#include "CompositeCommand.h"
+#include "MoraleCheckCommand.h"
+
+
+namespace c40kl
+{
+
+
+void EndPhaseCommand::GetPossibleCommands(const GameState& state, GameCommandArray& outCommands)
+{
+	//Construct composite command which performs a morale
+	// check for every unit which has taken damage,
+	// and then ends the phase:
+
+	GameCommandArray arr;
+
+	PositionArray allUnits = state.GetBoardState().GetAllUnits(0);
+	{
+		PositionArray restOfUnits = state.GetBoardState().GetAllUnits(1);
+		allUnits.insert(allUnits.end(), restOfUnits.begin(), restOfUnits.end());
+	}
+
+	for (const auto& unitPos : allUnits)
+	{
+		const auto& stats = state.GetBoardState().GetUnitOnSquare(unitPos);
+
+		if (stats.modelsLostThisPhase > 0)
+		{
+			//Need to book in a morale check!
+			arr.push_back(std::make_shared<MoraleCheckCommand>(unitPos));
+		}
+	}
+
+	//Follow all morale checks up with the end phase command
+	// then add the composite command to the output
+	arr.push_back(std::make_shared<EndPhaseCommand>());
+	outCommands.push_back(std::make_shared<CompositeCommand>(arr.begin(), arr.end()));
+}
+
+
+void EndPhaseCommand::Apply(const GameState& state,
+	std::vector<GameState>& outStates, std::vector<float>& outDistribution) const
+{
+	auto board = state.GetBoardState();
+
+	//Get list of all units
+	PositionArray allUnits = state.GetBoardState().GetAllUnits(0);
+	{
+		PositionArray restOfUnits = state.GetBoardState().GetAllUnits(1);
+		allUnits.insert(allUnits.end(), restOfUnits.begin(), restOfUnits.end());
+	}
+
+	//Check if about to change turn
+	const bool bChangingTurn = (state.GetPhase() == Phase::FIGHT);
+
+	//Reset turn-specific info for all units
+	for (const auto& unitPos : allUnits)
+	{
+		Unit stats = board.GetUnitOnSquare(unitPos);
+
+		//Things to reset for this phase:
+
+		stats.modelsLostThisPhase = 0;
+
+		//Things to reset for this turn:
+		if (bChangingTurn)
+		{
+			stats.attemptedChargeThisTurn = false;
+			stats.firedThisTurn = false;
+			stats.foughtThisTurn = false;
+			stats.movedOutOfCombatThisTurn = false;
+			stats.movedThisTurn = false;
+			stats.successfulChargeThisTurn = false;
+		}
+
+		//Update changes:
+		board.SetUnitOnSquare(unitPos, stats, board.GetTeamOnSquare(unitPos));
+	}
+
+	//Determine the next phase and team:
+	int nextTeam = state.GetTeam();
+	Phase nextPhase = state.GetPhase();
+	switch (nextPhase)
+	{
+	case Phase::MOVEMENT:
+		nextPhase = Phase::SHOOTING;
+		break;
+	case Phase::SHOOTING:
+		nextPhase = Phase::CHARGE;
+		break;
+	case Phase::CHARGE:
+		nextPhase = Phase::FIGHT;
+		break;
+	case Phase::FIGHT:
+		nextPhase = Phase::MOVEMENT;
+		nextTeam = 1 - nextTeam;
+		break;
+	default:
+		C40KL_ASSERT_INVARIANT(false, "Invalid game phase! Corrupted memory?");
+	}
+
+	//Create next game state
+	outStates.emplace_back(nextTeam, nextPhase, board);
+	outDistribution.push_back(1.0f);
+}
+
+
+bool EndPhaseCommand::Equals(const IGameCommand& cmd) const
+{
+	if (auto pCmd = dynamic_cast<const EndPhaseCommand*>(&cmd))
+	{
+		return true;
+	}
+	else return false;
+}
+
+
+String EndPhaseCommand::ToString() const
+{
+	return std::string("end phase command");
+}
+
+
+} // namespace c40kl
+
+
---
+++ Core40KLearn/EndPhaseCommand.h
@@ -0,0 +1,38 @@
+#pragma once
+
+
+#include "IGameCommand.h"
+
+
+namespace c40kl
+{
+
+
+class EndPhaseCommand :
+	public IGameCommand
+{
+public:
+	/// <summary>
+	/// This function gets all possible end phase commands
+	/// available in the given state and pushes them onto
+	/// the outCommands array. Note that due to the nature
+	/// of this command, there is always exactly one such
+	/// command available to the player.
+	/// </summary>
+	/// <param name="state">The state to issue commands in</param>
+	/// <param name="outCommands">The array that contains the command output.</param>
+	static void GetPossibleCommands(const GameState& state, GameCommandArray& outCommands);
+
+public:
+	//Interface functions
+
+	virtual void Apply(const GameState& state, std::vector<GameState>& outStates,
+		std::vector<float>& outDistribution) const override;
+	virtual bool Equals(const IGameCommand& cmd) const override;
+	virtual String ToString() const override;
+};
+
+
+} // namespace c40kl
+
+
